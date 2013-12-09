969a0cac0034bb37f3a3ef7aed62aff40e7f8a38
melondonkey <joedean83@gmail.com>
2013-12-09 16:43:01 -0500

    Update fifty19.jl

--- fifty19.jl
+++ fifty19.jl
@@ -1,33 +1,14 @@
 #19.  Isaac Newton helps Samuel Pepys
 
-#A) At least 1 6 with 6 dice
 
-function oneD(rolls)
+function newtonDice(dice, rolls)
   results = falses(rolls)
   for i in 1:rolls
-     results[i] = sum(rand(1:6,6) .== 6) >= 1
-  end
-  return mean(results)  #This is where I finally remembered mean of a binary vector is the way to go.
-end
-
-#B) 2 sixes with 12 dice
-function twoD(rolls)
-  results = falses(rolls)
-  for i in 1:rolls
-    results[i] = sum(rand(1:6,12) .== 6) >= 2
-  end
-  return mean(results)
-end
-
-#C) 3 sixes with 18 dice
-function threeD(rolls)
-  results = falses(rolls)
-  for i in 1:rolls
-          results[i] = sum(rand(1:6,18) .== 6) >= 3
+          results[i] = sum(rand(1:6,dice*6) .== 6) >= dice
   end
   return mean(results)
 end
 
 
-oneD(10000), twoD(10000), threeD(10000)
+newtonDice(1,10000), newtonDice(2,100000), newtonDice(3,10000)
 
